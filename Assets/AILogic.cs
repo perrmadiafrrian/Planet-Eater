cfd8f6417c853f9cbda90487ecc62338bd11b6d9
perrmadiafrrian <permadi.afrian@gmail.com>
2016-08-20 18:28:31 +0700

    AILogic (Not Yet Completed)
    wait to create spawner

---
+++ Assets/AILogic.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AILogic : MonoBehaviour {
+
+	private bool chasingOther;
+	private float rad;
+	public LayerMask planetMask;
+
+	// Use this for initialization
+	void Start () {
+		chasingOther = false;
+		rad = transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!chasingOther) {
+			SearchPlanet(transform.position, rad * .2f);
+		}
+	}
+
+	void SearchPlanet(Vector3 center, float radius) {
+		Collider[] hitColliders = Physics.OverlapSphere (center, radius, planetMask);
+		int i = 0;
+		bool found = false;
+		rad = radius;
+		while (i < hitColliders.Length && !found) {
+			if (hitColliders [i].transform.localScale.x < transform.localScale.x * .75f) {
+				found = true;
+				Debug.Log ("Chasing");
+				StartCoroutine (Chasing (hitColliders [i].gameObject));
+				rad = transform.localScale.x;
+			}
+		}
+	}
+
+	//SABTU 20 AGUSTUS 2016
+	IEnumerator Chasing(GameObject target) {
+		chasingOther = true;
+		yield return null;
+	}
+}
