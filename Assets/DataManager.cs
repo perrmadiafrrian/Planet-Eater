46ac8152d3a3a8844e5110b67ae5c1411a657ad0
isnainul <isnainul.fahrizal@student.uny.ac.id>
2016-09-05 07:34:23 +0700

    Main Menu

---
+++ Assets/DataManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataManager : MonoBehaviour {
+
+	public const string VRMode = "VRMode";
+
+	void Start () {
+	
+	}
+
+	public void SetVRMode (bool enabled) {
+		PlayerPrefs.SetInt (VRMode,enabled?1:0);
+	}
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	GvrViewer googleVR;
+	void Start () {
+		googleVR = GameObject.FindObjectOfType<GvrViewer> ();
+		if (googleVR != null) {
+			googleVR.VRModeEnabled = PlayerPrefs.GetInt (DataManager.VRMode,0) == 0 ? false : true;
+		}
+	}
+
+	public void Play() {
+		AsyncOperation ao = SceneManager.LoadSceneAsync ("Tes");
+		ao.allowSceneActivation = true;
+	}
+
+	void Update () {
+	
+	}
+}
